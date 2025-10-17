6cf86c4d921359ca7d7efcc02a7cb83408e6c622
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-10-17 18:05:01 +0300

    Products Page

--- assets/jss/allProducts.js
+++ assets/jss/allProducts.js
@@ -12,19 +12,21 @@ const displayProducts=async(page=1)=>{
     Products = response.data.products;
     const numberOfPages= Math.ceil(response.data.total / 15);
     const result= response.data.products.map(product=>`
-        <div class="col-md-3 my-3">
-        <div class="card -d-flex  flex-column" style="width: 18rem;">
+       <div class="card -d-flex  flex-column" style="width: 18rem;">
   <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
   <div class="card-body">
-    <h5 class="card-title subColor2 textShadow">${product.title}</h5>
-    <div class='d-flex gap-5'>
+    <h5 class="card-title subColor3 subFont">${product.title}</h5>
+    <div class='d-flex gap-5 subFont1'>
     <span class='flex-grow-1'>${product.price}$</span>
     <span class=''><i class="fa-solid fa-star" style="color: #FFD43B;"></i>
     ${product.rating}</span>
     </div>
-    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor" target="_blank">Details</a>
+    <div class="cartBtn-heartBtn d-flex gap-2 mt-3 subFont">
+    <button class='btn bg-subColor1'><i class="fa-solid fa-heart fa-lg" style="color: #920001;"></i></button>
+    <button class='btn bg-subColor1 subColor3 flex-grow-1'><i class="fa-solid fa-cart-shopping fa-lg" style="color: #920001;"></i> Add to Cart</button>
+    </div>
+    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor subFont" target="_blank">Details</a>
   </div>
-</div>
 </div>
         `
     ).join (' ');
@@ -67,19 +69,51 @@ searchInput.addEventListener("input", ()=>{
 });
      const result = filteredProducts.map ((product)=>{
         return `
-        <div class="col-md-3 my-3">
         <div class="card -d-flex  flex-column" style="width: 18rem;">
   <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
   <div class="card-body">
-    <h5 class="card-title subColor2 textShadow">${product.title}</h5>
-    <div class='d-flex gap-5'>
+    <h5 class="card-title subColor3 subFont">${product.title}</h5>
+    <div class='d-flex gap-5 subFont1'>
     <span class='flex-grow-1'>${product.price}$</span>
     <span class=''><i class="fa-solid fa-star" style="color: #FFD43B;"></i>
     ${product.rating}</span>
     </div>
-    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor" target="_blank">Details</a>
+    <div class="cartBtn-heartBtn d-flex gap-2 mt-3 subFont">
+    <button class='btn bg-subColor1'><i class="fa-solid fa-heart fa-lg" style="color: #920001;"></i></button>
+    <button class='btn bg-subColor1 subColor3 flex-grow-1'><i class="fa-solid fa-cart-shopping fa-lg" style="color: #920001;"></i> Add to Cart</button>
+    </div>
+    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor subFont" target="_blank">Details</a>
   </div>
 </div>
+        `
+
+    }).join('');
+    document.querySelector(".productsContent").innerHTML=result;
+})
+/* nav search */
+const navSearchInput = document.querySelector(".navSearchInput");
+navSearchInput.addEventListener("input", ()=>{
+    const filterText= navSearchInput.value.toLowerCase();
+    const filteredProducts = Products.filter((product)=>{
+    return product.title.toLowerCase().includes(filterText);
+});
+     const result = filteredProducts.map ((product)=>{
+        return `
+        <div class="card -d-flex  flex-column" style="width: 18rem;">
+  <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
+  <div class="card-body">
+    <h5 class="card-title subColor3 subFont">${product.title}</h5>
+    <div class='d-flex gap-5 subFont1'>
+    <span class='flex-grow-1'>${product.price}$</span>
+    <span class=''><i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+    ${product.rating}</span>
+    </div>
+    <div class="cartBtn-heartBtn d-flex gap-2 mt-3 subFont">
+    <button class='btn bg-subColor1'><i class="fa-solid fa-heart fa-lg" style="color: #920001;"></i></button>
+    <button class='btn bg-subColor1 subColor3 flex-grow-1'><i class="fa-solid fa-cart-shopping fa-lg" style="color: #920001;"></i> Add to Cart</button>
+    </div>
+    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor subFont" target="_blank">Details</a>
+  </div>
 </div>
         `
 
@@ -213,3 +247,17 @@ deleteBtn.addEventListener("click", async()=>{
   }
 })
 /* Delete Product */
+
+
+/* Nav Btns */
+const navSearchBtn=document.querySelector(".fa-magnifying-glass");
+const navCartBtn=document.querySelector(".fa-cart-shopping");
+const navHeartBtn=document.querySelector(".fa-heart");
+const navUserBtn=document.querySelector(".fa-user");
+const offcanvasCart=document.querySelector(".offcanvasCart");
+
+navSearchBtn.addEventListener("click", ()=>{
+   navSearchInput.classList.remove('d-none');
+})
+
+/* Nav Btns */
--- assets/jss/script.js
+++ assets/jss/script.js
@@ -7,17 +7,22 @@ const getProducts=async()=>{
 getProducts();
 const displayProducts=async()=>{
     const response= await getProducts();
+    Products = response.data.products;
     const result= response.data.products.map(product=>`
         <div class="card -d-flex  flex-column" style="width: 18rem;">
   <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
   <div class="card-body">
-    <h5 class="card-title subColor2 textShadow">${product.title}</h5>
-    <div class='d-flex gap-5'>
+    <h5 class="card-title subColor3 subFont">${product.title}</h5>
+    <div class='d-flex gap-5 subFont1'>
     <span class='flex-grow-1'>${product.price}$</span>
     <span class=''><i class="fa-solid fa-star" style="color: #FFD43B;"></i>
     ${product.rating}</span>
     </div>
-    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor" target="_blank">Details</a>
+    <div class="cartBtn-heartBtn d-flex gap-2 mt-3 subFont">
+    <button class='btn bg-subColor1'><i class="fa-solid fa-heart fa-lg" style="color: #920001;"></i></button>
+    <button class='btn bg-subColor1 subColor3 flex-grow-1'><i class="fa-solid fa-cart-shopping fa-lg" style="color: #920001;"></i> Add to Cart</button>
+    </div>
+    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor subFont" target="_blank">Details</a>
   </div>
 </div>
      
@@ -39,9 +44,9 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
 
-  autoplay: {
+  /* autoplay: {
     deley:900,
-  },
+  }, */
 
   effect: 'fade',
   speed: 1500,
@@ -74,5 +79,49 @@ displayCategory();
 /* Home-Categories end */
 
 
+/* Nav Btns */
+const navSearchBtn=document.querySelector(".fa-magnifying-glass");
+const navCartBtn=document.querySelector(".fa-cart-shopping");
+const navHeartBtn=document.querySelector(".fa-heart");
+const navUserBtn=document.querySelector(".fa-user");
 
+const offcanvasCart=document.querySelector(".offcanvasCart");
+
+navSearchBtn.addEventListener("click", ()=>{
+   navSearchInput.classList.remove('d-none');
+})
+
+
+/* nav search */
+const navSearchInput = document.querySelector(".navSearchInput");
+navSearchInput.addEventListener("input", ()=>{
+    const filterText= navSearchInput.value.toLowerCase();
+    const filteredProducts = Products.filter((product)=>{
+    return product.title.toLowerCase().includes(filterText);
+});
+     const result = filteredProducts.map ((product)=>{
+        return `
+        <div class="card -d-flex  flex-column" style="width: 18rem;">
+  <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
+  <div class="card-body">
+    <h5 class="card-title subColor3 subFont">${product.title}</h5>
+    <div class='d-flex gap-5 subFont1'>
+    <span class='flex-grow-1'>${product.price}$</span>
+    <span class=''><i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+    ${product.rating}</span>
+    </div>
+    <div class="cartBtn-heartBtn d-flex gap-2 mt-3 subFont">
+    <button class='btn bg-subColor1'><i class="fa-solid fa-heart fa-lg" style="color: #920001;"></i></button>
+    <button class='btn bg-subColor1 subColor3 flex-grow-1'><i class="fa-solid fa-cart-shopping fa-lg" style="color: #920001;"></i> Add to Cart</button>
+    </div>
+    <a href="./prodDetails.html?prodID=${product.id}" class="btn prodBtn w-100 mt-2 subColor1 bg-mainColor subFont" target="_blank">Details</a>
+  </div>
+</div>
+        `
+
+    }).join('');
+    document.querySelector(".homeProductsCard").innerHTML=result;
+})
+
+/* Nav Btns */
 
